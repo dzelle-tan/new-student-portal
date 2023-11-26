8d7524ef19fe78822c12e47ed0e1d3146f992d79
dzelle-tan <dzellefaith@gmail.com>
2023-11-26 14:35:59 +0800

    [feature][dz] - add navigation

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -32,8 +32,64 @@ new class extends Component
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                        <x-icon name="home" class="w-5 h-5 mr-2" solid/> {{ __('Home') }}
                     </x-nav-link>
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md bg-white focus:outline-none transition ease-in-out duration-150">
+                                    <x-icon name="identification" class="w-5 h-5 mr-2" solid/>
+                                    <div x-data="{ name: 'Information' }" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
+        
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+        
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile')" wire:navigate>
+                                    {{ __('Classes') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('profile')" wire:navigate>
+                                    {{ __('Grades') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('profile')" wire:navigate>
+                                    {{ __('Violations') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <x-icon name="building-office-2" class="w-5 h-5 mr-2" solid/>
+                                    <div x-data="{ name: 'Services' }" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
+        
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+        
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile')" wire:navigate>
+                                    {{ __('Enrollment') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('profile')" wire:navigate>
+                                    {{ __('Registrar') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('profile')" wire:navigate>
+                                    {{ __('Evaluation') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 </div>
             </div>
 
--- resources/views/livewire/profile/show-student-information.blade.php
+++ resources/views/livewire/profile/show-student-information.blade.php
@@ -68,11 +68,11 @@ new class extends Component
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+            {{ __('Student Information') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("If the degree program is erroneous, please contact your respective college's OCS. If all details shown here are correct, you may proceed to the Pre-enlistment.") }}
         </p>
     </header>
 
@@ -105,14 +105,6 @@ new class extends Component
                     @endif
                 </div>
             @endif
-        </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            <x-action-message class="me-3" on="profile-updated">
-                {{ __('Saved.') }}
-            </x-action-message>
         </div>
     </form>
 </section>
