dde151a0f1385d27c8a420acc6abfc714df8d8ec
Vivien <samanthaservo09@gmail.com>
2023-11-30 22:28:49 +0800

    [student violations data][sam] - added basic db columns

--- database/seeders/StudentViolation.php
+++ database/seeders/StudentViolation.php
@@ -12,6 +12,80 @@ class StudentViolation extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('student_violations')->insert([
+            'id' => 1,
+            'student_id' => 1,
+            'violation' => 'Improper Uniform Attire',
+            'violation_date' => '',
+            'offense_type' => 'Programming Languages (LAB)',
+            'sm_reference' => '',
+            'resolution' => 'MWF',
+            'resolution_remarks' => '14:00:00',
+            'resolution_date' => '17:00:00',
+            'status' => 'Gusaling Villegas',
+            'created_at' => 'GV306',
+            'updated_at' => 'GV306',
+        ]);
+
+        DB::table('student_violations')->insert([
+            'id' => 1,
+            'student_id' => 1,
+            'violation' => 'Improper Uniform Attire',
+            'violation_date' => '',
+            'offense_type' => 'Programming Languages (LAB)',
+            'sm_reference' => 3,
+            'resolution' => 'MWF',
+            'resolution_remarks' => '14:00:00',
+            'resolution_date' => '17:00:00',
+            'status' => 'Gusaling Villegas',
+            'created_at' => 'GV306',
+            'updated_at' => 'GV306',
+        ]);
+
+        DB::table('student_violations')->insert([
+            'id' => 1,
+            'student_id' => 1,
+            'violation' => 'Improper Uniform Attire',
+            'violation_date' => '',
+            'offense_type' => 'Programming Languages (LAB)',
+            'sm_reference' => 3,
+            'resolution' => 'MWF',
+            'resolution_remarks' => '14:00:00',
+            'resolution_date' => '17:00:00',
+            'status' => 'Gusaling Villegas',
+            'created_at' => 'GV306',
+            'updated_at' => 'GV306',
+        ]);
+
+        DB::table('student_violations')->insert([
+            'id' => 1,
+            'student_id' => 1,
+            'violation' => 'Improper Uniform Attire',
+            'violation_date' => '',
+            'offense_type' => 'Programming Languages (LAB)',
+            'sm_reference' => 3,
+            'resolution' => 'MWF',
+            'resolution_remarks' => '14:00:00',
+            'resolution_date' => '17:00:00',
+            'status' => 'Gusaling Villegas',
+            'created_at' => 'GV306',
+            'updated_at' => 'GV306',
+        ]);
+
+
+        DB::table('student_violations')->insert([
+            'id' => 1,
+            'student_id' => 1,
+            'violation' => 'Improper Uniform Attire',
+            'violation_date' => '',
+            'offense_type' => 'Programming Languages (LAB)',
+            'sm_reference' => 3,
+            'resolution' => 'MWF',
+            'resolution_remarks' => '14:00:00',
+            'resolution_date' => '17:00:00',
+            'status' => 'Gusaling Villegas',
+            'created_at' => 'GV306',
+            'updated_at' => 'GV306',
+        ]);
     }
 }
