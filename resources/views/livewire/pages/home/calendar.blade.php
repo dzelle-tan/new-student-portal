c090ebacd289aa65e219bb064ce25bd788d531cc
dzelle-tan <dzellefaith@gmail.com>
2023-12-03 22:39:25 +0800

    [misc][dz] - emeng calendar

--- resources/views/livewire/pages/home/calendar.blade.php
+++ resources/views/livewire/pages/home/calendar.blade.php
@@ -6,24 +6,121 @@ new class extends Component {
     //
 }; ?>
 
-<div>
+<div class="h-[33rem]">
     <h3 class="text-lg font-medium">{{__("University Calendar")}}</h3>
-    <div class="w-full mt-4 overflow-x-auto">
+    <div class="w-full max-h-full mt-4 overflow-x-auto overflow-y-auto">
         <table class="w-full text-left whitespace-nowrap">
             <thead>
                 <tr class="text-xs tracking-wider uppercase border-b border-gray-200 text-table-header bg-gray-50">
                     <th class="px-4 py-3 font-medium">Event</th>
-                    <th class="px-4 py-3 font-medium">First Semester</th>
-                    <th class="px-4 py-3 font-medium">Second Semester</th>
-                    <th class="px-4 py-3 font-medium">Midyear Term</th>
+                    <th class="px-4 py-3 font-medium">
+                        First Semester
+                        <p class="text-xs font-normal normal-case">Aug 29, 2023 - Jan 16, 2024</p>
+                    </th>
+                    <th class="px-4 py-3 font-medium">
+                        Second Semester
+                        <p class="text-xs font-normal normal-case">Feb 5, 2024 - Jun 9, 2024</p>
+                    </th>
+                    <th class="px-4 py-3 font-medium">
+                        Midyear Term
+                        <p class="text-xs font-normal normal-case">Jul 1, 2024 - Aug 10, 2024</p>
+                    </th>
                 </tr>
             </thead>
             <tbody>
                 <tr class="text-sm border-b border-gray-200">
-                    <td class="px-4 py-3"></td>
-                    <td class="px-4 py-3"></td>
-                    <td class="px-4 py-3"></td>
-                    <td class="px-4 py-3"></td>
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Registration Period
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Late Registration
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Start of Classes
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Add-Drop Period
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Mid-Term Examinations
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Christmas Vacation
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">-</td>
+                    <td class="px-4 py-3">-</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        End of Classes
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Final Examination
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Encoding of Grades
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        Last Day of Submitting Report of Grades
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm border-b border-gray-200">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">
+                        University Foundation Anniversary
+                    </th>
+                    <td class="px-4 py-3">Jul 31(M) - Aug 19(S), 2023</td>
+                    <td class="px-4 py-3">Jan 24(W) - Jan 30(T), 2024</td>
+                    <td class="px-4 py-3">Jun 25(T) - Jun 29(S), 2024</td>
+                </tr>
+                <tr class="text-sm">
+                    <th class="max-w-xs px-4 py-3 overflow-auto font-medium text-gray-800 whitespace-normal">Araw ng Maynila</th>
+                    <td class="px-4 py-3">-</td>
+                    <td class="px-4 py-3">Jun 24(M), 2024</td>
+                    <td class="px-4 py-3">-</td>
                 </tr>
             </tbody>
         </table>
