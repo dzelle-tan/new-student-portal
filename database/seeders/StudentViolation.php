cdf1b56d1676a0a12cad9f576b0358a35aa6e7a7
Vivien <samanthaservo09@gmail.com>
2023-12-04 08:29:25 +0800

    [student violations data][sam] - added additional import to run properly

--- database/seeders/StudentViolation.php
+++ database/seeders/StudentViolation.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class StudentViolation extends Seeder
