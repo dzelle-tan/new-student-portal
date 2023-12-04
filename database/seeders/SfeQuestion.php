e3ea82ee7c415be78f2b170820f2e2dc25a9d189
Vivien <samanthaservo09@gmail.com>
2023-12-04 22:35:15 +0800

    [sfe question data][sam] - added the timestamps

--- database/seeders/SfeQuestion.php
+++ database/seeders/SfeQuestion.php
@@ -15,65 +15,65 @@ class SfeQuestion extends Seeder
     {
         DB::table('sfe_questions')->insert([
             'question' => 'Explains the course objectives, expectations, and requirements of the course.',
-            'type' => 'Faculty Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Faculty Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'Comes to class unprepared for the lesson.',
-            'type' => 'Faculty Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Faculty Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'Presents the subject matter clearly and systematically.            ',
-            'type' => 'Faculty Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Faculty Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'This course stimulates me to study beyond the lessons assigned.',
-            'type' => 'Course Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Course Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'This course has developed in me a greater sense of responsibility. (i.e. self-reliance, self-discipline, independent study)',
-            'type' => 'Course Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Course Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'I have worked more conscientiously in this course than in most other courses.',
-            'type' => 'Course Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Course Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'How many times have you been late?',
-            'type' => 'Self Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Self Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'How many class meetings have you missed?',
-            'type' => 'Self Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Self Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         DB::table('sfe_questions')->insert([
             'question' => 'What final grade would you give yourself in this course?',
-            'type' => 'Self Assessment'
-            //created_at automatically updated to store the timestamps
-            //updated_at automatically updated to store the timestamps
+            'type' => 'Self Assessment',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
