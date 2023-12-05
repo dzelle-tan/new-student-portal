d2dce7282558d7378d94f5d99631d8429e0e4711
Vivien <samanthaservo09@gmail.com>
2023-12-05 22:45:17 +0800

    [sfe status data][sam] - added the seed data

--- database/seeders/SfeStatus.php
+++ database/seeders/SfeStatus.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class SfeStatus extends Seeder
@@ -12,6 +13,54 @@ class SfeStatus extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('classes')->insert([
+            'student_id' => 1,
+            'class_id' => 1,
+            'aysem' => '20231',
+            'status' => 'Completed',
+            'date_finished' => '2023-12-11 22:35:58',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('classes')->insert([
+            'student_id' => 2,
+            'class_id' => 1,
+            'aysem' => '20231',
+            'status' => 'Completed',
+            'date_finished' => '2023-12-15 08:13:00',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('classes')->insert([
+            'student_id' => 3,
+            'class_id' => 1,
+            'aysem' => '20231',
+            'status' => 'Completed',
+            'date_finished' => '2023-12-15 12:35:00',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('classes')->insert([
+            'student_id' => 4,
+            'class_id' => 1,
+            'aysem' => '20231',
+            'status' => 'Completed',
+            'date_finished' => '2023-12-11 20:15:00',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('classes')->insert([
+            'student_id' => 1,
+            'class_id' => 2,
+            'aysem' => '20231',
+            'status' => 'Completed',
+            'date_finished' => '2023-12-11 22:17:00',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
