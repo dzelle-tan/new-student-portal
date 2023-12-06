76b4fcae0bf4e75efc93a7bfcfd0ef88498027a1
mebcrtz <merlindabinalla@gmail.com>
2023-12-06 21:59:09 +0800

    [feature][meb] models and migrations

---
+++ database/seeders/Semester.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Semester extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
