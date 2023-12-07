8c0255e25be86cdb039dd11730da576119083333
dzelle-tan <dzellefaith@gmail.com>
2023-12-07 20:21:48 +0800

    [misc][dz] change label to plm email

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -37,7 +37,7 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <h2 class="text-primary font-semibold text-2xl mt-2">Forgot Password</h2>
+    <h2 class="mt-2 text-2xl font-semibold text-primary">Forgot Password</h2>
     <div class="my-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -48,8 +48,8 @@ new #[Layout('layouts.guest')] class extends Component
     <form wire:submit="sendPasswordResetLink">
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
+            <x-input-label for="email" :value="__('PLM Email')" />
+            <x-text-input wire:model="email" id="email" class="block w-full mt-1" type="email" name="email" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
