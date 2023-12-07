d8c3500b4d9d85ef1575b4cdfd389c2bd994e6d9
dzelle-tan <dzellefaith@gmail.com>
2023-12-07 16:22:15 +0800

    fix display classes

--- resources/views/livewire/pages/classes/show.blade.php
+++ resources/views/livewire/pages/classes/show.blade.php
@@ -30,12 +30,12 @@ new class extends Component {
 <div class="grid grid-cols-1 overflow-hidden bg-white shadow-sm sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 rounded-md lg:min-h-[38rem]">
     @foreach ($daysOfWeek as $dayName)
         <div class="-mr-px border">
-            <div class="px-6 py-4 text-sm font-medium tracking-wider uppercase border-b-2 text-table-header bg-gray-50">
+            <div class="px-6 py-4 mb-1 text-sm font-medium tracking-wider uppercase border-b-2 text-table-header bg-gray-50">
                 {{ $dayName }}
             </div>
             @foreach ($classes as $class)
                 @if ($class->day == $dayName)
-                    <div class="p-2 {{ $loop->first ? '' : 'pt-0' }}">
+                    <div class="p-2 pt-1">
                         <x-class-block 
                             time="{{ $this->formatTime($class->start_time) }} - {{ $this->formatTime($class->end_time) }}"  
                             code="{{ $class->code }}" 
