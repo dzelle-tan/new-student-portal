9874b89abd418d60f45e4646f80c1c6700b487e7
dzelle-tan <dzellefaith@gmail.com>
2023-12-07 16:23:14 +0800

    order of display

--- resources/views/livewire/pages/registrar/show.blade.php
+++ resources/views/livewire/pages/registrar/show.blade.php
@@ -163,7 +163,7 @@ new class extends Component
             <div class="absolute left-0 top-2/4 h-0.5 w-full -translate-y-2/4 bg-gray-300"></div>
             <div class="absolute left-0 top-2/4 h-0.5 {{ $current_step == 2 ? 'w-1/2' : ($current_step == 3 ? 'w-full' : '') }} -translate-y-2/4 bg-primary transition-all duration-500">
             </div>
-            <div class="relative z-10 grid w-10 h-10 font-bold text-white transition-all duration-300 rounded-full place-items-center bg-primary">
+            <div class="relative grid w-10 h-10 font-bold text-white transition-all duration-300 rounded-full z-5 place-items-center bg-primary">
                 1
                 <div class="absolute -bottom-[2rem] w-max text-center">
                 <h6 class="block font-sans text-base antialiased font-medium leading-relaxed tracking-normal text-gray-700">
@@ -171,7 +171,7 @@ new class extends Component
                 </h6>
                 </div>
             </div>
-            <div class="relative z-10 grid w-10 h-10 font-bold transition-all duration-300 rounded-full place-items-center {{ $current_step >= 2 ? ' text-white bg-primary' : 'bg-gray-300 text-gray-900' }}">
+            <div class="relative z-5 grid w-10 h-10 font-bold transition-all duration-300 rounded-full place-items-center {{ $current_step >= 2 ? ' text-white bg-primary' : 'bg-gray-300 text-gray-900' }}">
                 2
                 <div class="absolute -bottom-[2rem] w-max text-center">
                     <h6 class="block font-sans text-base antialiased font-medium leading-relaxed tracking-normal text-gray-900">
@@ -179,7 +179,7 @@ new class extends Component
                     </h6>
                 </div>
             </div>
-            <div class="relative z-10 grid w-10 h-10 font-bold transition-all duration-300 rounded-full place-items-center {{ $current_step == 3 ? ' text-white bg-primary' : 'bg-gray-300 text-gray-900' }}">
+            <div class="relative z-5 grid w-10 h-10 font-bold transition-all duration-300 rounded-full place-items-center {{ $current_step == 3 ? ' text-white bg-primary' : 'bg-gray-300 text-gray-900' }}">
                 3
                 <div class="absolute -bottom-[2rem] w-max text-center">
                 <h6 class="block font-sans text-base antialiased font-medium leading-relaxed tracking-normal text-gray-700">
