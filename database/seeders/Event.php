c9f95fedab2101bf9ff313f0065e213f0904d555
dzelle-tan <dzellefaith@gmail.com>
2023-12-10 14:13:28 +0800

    seeder

--- database/seeders/Event.php
+++ database/seeders/Event.php
@@ -16,16 +16,16 @@ class Event extends Seeder
         DB::table('events')->insert([
             'semester_id' => 1,
             'event_name' => 'Registration Period',
-            'start_date' => '2023-08-29',
-            'end_date' => '2024-01-16',
+            'start_date' => '2023-07-31',
+            'end_date' => '2023-08-19',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 1,
             'event_name' => 'Late Registration',
-            'start_date' => '2023-08-29',
-            'end_date' => '2024-01-16',
+            'start_date' => '2023-08-22',
+            'end_date' => '2023-08-26',
             'created_at' => now(),
         ]);
 
@@ -33,7 +33,7 @@ class Event extends Seeder
             'semester_id' => 1,
             'event_name' => 'Start of Classes',
             'start_date' => '2023-08-29',
-            'end_date' => '2024-01-16',
+            'end_date' => '',
             'created_at' => now(),
         ]);
 
@@ -41,23 +41,23 @@ class Event extends Seeder
             'semester_id' => 1,
             'event_name' => 'Add-Drop Period',
             'start_date' => '2023-08-29',
-            'end_date' => '2024-01-16',
+            'end_date' => '2023-09-02',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 1,
             'event_name' => 'Mid-Term Examinations',
-            'start_date' => '2023-08-29',
-            'end_date' => '2024-01-16',
+            'start_date' => '2023-10-23',
+            'end_date' => '2023-10-29',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 1,
             'event_name' => 'Christmas Vacation',
-            'start_date' => '2023-08-29',
-            'end_date' => '2024-01-16',
+            'start_date' => '2023-12-18',
+            'end_date' => '2024-01-01',
             'created_at' => now(),
         ]);
 
@@ -116,16 +116,16 @@ class Event extends Seeder
         DB::table('events')->insert([
             'semester_id' => 2,
             'event_name' => 'Registration Period',
-            'start_date' => '2024-02-05',
-            'end_date' => '2024-06-09',
+            'start_date' => '2024-01-24',
+            'end_date' => '2024-01-30',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 2,
             'event_name' => 'Late Registration',
-            'start_date' => '2024-02-05',
-            'end_date' => '2024-06-09',
+            'start_date' => '2024-01-31',
+            'end_date' => '2024-02-03',
             'created_at' => now(),
         ]);
 
@@ -133,7 +133,7 @@ class Event extends Seeder
             'semester_id' => 2,
             'event_name' => 'Start of Classes',
             'start_date' => '2024-02-05',
-            'end_date' => '2024-06-09',
+            'end_date' => '',
             'created_at' => now(),
         ]);
 
@@ -141,23 +141,23 @@ class Event extends Seeder
             'semester_id' => 2,
             'event_name' => 'Add-Drop Period',
             'start_date' => '2024-02-05',
-            'end_date' => '2024-06-09',
+            'end_date' => '2024-02-10',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 2,
             'event_name' => 'Mid-Term Examinations',
-            'start_date' => '2024-02-05',
-            'end_date' => '2024-06-09',
+            'start_date' => '2024-04-01',
+            'end_date' => '2024-04-07',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 2,
             'event_name' => 'Christmas Vacation',
-            'start_date' => '2024-02-05',
-            'end_date' => '2024-06-09',
+            'start_date' => '',
+            'end_date' => '',
             'created_at' => now(),
         ]);
 
@@ -218,8 +218,8 @@ class Event extends Seeder
         DB::table('events')->insert([
             'semester_id' => 3,
             'event_name' => 'Registration Period',
-            'start_date' => '2024-07-01',
-            'end_date' => '2024-08-10',
+            'start_date' => '2024-06-25',
+            'end_date' => '2024-06-29',
             'created_at' => now(),
         ]);
 
@@ -227,7 +227,7 @@ class Event extends Seeder
             'semester_id' => 3,
             'event_name' => 'Late Registration',
             'start_date' => '2024-07-01',
-            'end_date' => '2024-08-10',
+            'end_date' => '2024-07-06',
             'created_at' => now(),
         ]);
 
@@ -235,7 +235,7 @@ class Event extends Seeder
             'semester_id' => 3,
             'event_name' => 'Start of Classes',
             'start_date' => '2024-07-01',
-            'end_date' => '2024-08-10',
+            'end_date' => '',
             'created_at' => now(),
         ]);
 
@@ -243,23 +243,23 @@ class Event extends Seeder
             'semester_id' => 3,
             'event_name' => 'Add-Drop Period',
             'start_date' => '2024-07-01',
-            'end_date' => '2024-08-10',
+            'end_date' => '2024-07-01',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 3,
             'event_name' => 'Mid-Term Examinations',
-            'start_date' => '2024-07-01',
-            'end_date' => '2024-08-10',
+            'start_date' => '2024-07-18',
+            'end_date' => '2024-07-20',
             'created_at' => now(),
         ]);
 
         DB::table('events')->insert([
             'semester_id' => 3,
             'event_name' => 'Christmas Vacation',
-            'start_date' => '2024-07-01',
-            'end_date' => '2024-08-10',
+            'start_date' => '',
+            'end_date' => '',
             'created_at' => now(),
         ]);
 
