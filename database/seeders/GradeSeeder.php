ca42a58230e23f618bb4bc6958111a37c2262ba9
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-06-05 22:01:41 +0800

    classes and grades seeders

--- database/seeders/GradeSeeder.php
+++ database/seeders/GradeSeeder.php
@@ -8,42 +8,36 @@ use Illuminate\Database\Seeder;
 
 class GradeSeeder extends Seeder
 {
-    /**
+/**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $grades = [
-            [
-                'remarks' => 'Passed',
-                'grade' => 1.00,
-                'completion_grade' => null,
-                'submitted_date' => now(),
-                'finalization_date' => now(),
-                'class_id' => 1,
-                'student_no' => 202310001,
-            ],
-            [
-                'remarks' => 'Failed',
-                'grade' => 5.00,
-                'completion_grade' => null,
-                'submitted_date' => now(),
-                'finalization_date' => now(),
-                'class_id' => 2,
-                'student_no' => 202310001,
-            ],
-            [
-                'remarks' => 'INC',
-                'grade' => null,
-                'completion_grade' => 2.00,
-                'submitted_date' => now(),
-                'finalization_date' => now(),
-                'class_id' => 1,
-                'student_no' => 202310001,
-            ],
-        ];
+        $grades = [1.00, 1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 2.75];
+        $remarksOptions = ['Passed']; // 'Failed', 'INC'
 
-        foreach ($grades as $grade) {
+        $data = [];
+
+        // Generating grades for classes 1 to 47 for multiple students
+        for ($classId = 1; $classId <= 7; $classId++) {
+            $remarks = $remarksOptions[array_rand($remarksOptions)];
+            $grade = $grades[array_rand($grades)];
+            $completionGrade = $grades[array_rand($grades)];
+
+            $data[] = [
+                'remarks' => $remarks,
+                'grade' => $grade,
+                'completion_grade' => $completionGrade,
+                'submitted_date' => now(),
+                'finalization_date' => now(),
+                'class_id' => $classId,
+                'student_no' => 202310001,
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+
+        foreach ($data as $grade) {
             Grade::create($grade);
         }
     }
