2b6cd59977e38bca873ffdc85cde72df7a179400
dzelle-tan <dzellefaith@gmail.com>
2024-06-10 00:57:52 +0800

    remove animation in name

--- resources/views/livewire/pages/home/name.blade.php
+++ resources/views/livewire/pages/home/name.blade.php
@@ -15,10 +15,6 @@ new class extends Component {
 <div class="flex items-center">
     <x-icon name="fire" class="w-6 h-6 text-secondary animate-pulse"/>
     <x-icon name="building-library" class="w-6 h-6 text-primary-light-2"/>
-    <h2 class="flex items-center relative w-[max-content]
-        before:absolute before:inset-0 before:animate-typewriter
-        before:bg-white
-        after:absolute after:inset-0 after:w-[0.125em] after:animate-caret
-        after:bg-black text-lg ml-3">Welcome,<span class="ml-1 font-medium">{{ $user->first_name }} {{ $user->last_name }}!</span>
+    <h2 class="flex items-center relative w-[max-content] text-lg ml-3">Welcome,<span class="ml-1 font-medium">{{ $user->first_name }} {{ $user->last_name }}!</span>
     </h2>
 </div>
